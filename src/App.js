4eb4cc74f275b346d476f8b1dcb93f8f23fb085c
softrain <yurimplus@naver.com>
2021-07-07 01:46:23 +0900

    3.4.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 //함수형
 import React from "react";
 import MyComponent from "./MyComponent";
-
+import Counter from "./Counter";
 const App = () => {
-  return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
 };
 export default App;
 
