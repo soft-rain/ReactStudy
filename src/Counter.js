0bb75adccb90cb348f2e03feb2c74455e2ea6b15
softrain <yurimplus@naver.com>
2021-07-07 01:51:18 +0900

    3.4.1.3

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    //state의 초기값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  state = { number: 0, fixedNumber: 0 };
   render() {
-    const { number } = this.state; //state를 조회할 때는 this.state로 조회한다.
+    const { number, fixedNumber } = this.state; //state를 조회할 때는 this.state로 조회한다.
 
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값:{fixedNumber}</h2>
         <button
           onClick={() => {
             //this.setState를 사용하여 stzte에 새로운 값을 넣을 수 있음.
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return { number: number + 1 };
+            });
+            this.setState((prevState) => ({ number: prevState.number + 1 }));
           }}
         >
           +1
