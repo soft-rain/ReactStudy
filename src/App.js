6c11950c8f2c1488f765cbb431f15fa87a6fda3b
softrain <yurimplus@naver.com>
2021-07-10 00:22:32 +0900

    4.2.2.1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from "react";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
+import EventPractice from "./EventPractice";
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 export default App;
 
