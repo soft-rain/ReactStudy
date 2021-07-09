77142ce436254a91c89bb1522382883d97e33bdf
softrain <yurimplus@naver.com>
2021-07-10 00:27:32 +0900

    4.2.3.1

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,6 +4,18 @@ class EventPractice extends Component {
   state = {
     message: "",
   };
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ message: e.target.value });
+  }
+  handleClick() {
+    alert(this.state.message);
+    this.setState({ message: "" });
+  }
   render() {
     return (
       <div>
@@ -13,18 +25,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({ message: e.target.value });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({ message: "" });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
