f101b99210672901f43d60b39b9afb751619b1c9
softrain <yurimplus@naver.com>
2021-07-10 00:32:01 +0900

    4.2.4

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,24 +2,28 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(e) {
-    this.setState({ message: e.target.value });
-  }
-  handleClick() {
-    alert(this.state.message);
-    this.setState({ message: "" });
-  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
+    this.setState({ username: "", message: "" });
+  };
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
         <input
           type="text"
           name="message"
