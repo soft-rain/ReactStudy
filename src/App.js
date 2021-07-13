2c57b059ce7ab8a1e3216b0cd11e871336f9ed0b
softrain <yurimplus@naver.com>
2021-07-13 19:55:18 +0900

    5.3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 //함수형
-import React from "react";
+import React, { Component } from "react";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
-const App = () => {
-  return <ValidationSample />;
-};
+import ScrollBox from "./ScrollBox";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
 
 //컴포넌트형
